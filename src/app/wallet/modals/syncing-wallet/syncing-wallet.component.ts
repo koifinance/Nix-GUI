f4efabe6606738d37bffdc4dcef2b74eaaab36ab
Silver Star <ryongak@yahoo.com>
2018-09-03 13:59:28 -0400

    07 > get wallet info encryption status, 08 > real time price data for 6.6344 BTC ($63,082) 09 > get wallet info “ghost_vault” 10 > nix price 11 > Price fetch, build chart 12 > Show Wallet sync status
    07 > get wallet info encryption status,
    08 > real time price data for 6.6344 BTC ($63,082)
    09 > get wallet info “ghost_vault”
    10 > nix price
    11 > Price fetch, build chart
    12 > Show Wallet sync status

--- src/app/wallet/modals/syncing-wallet/syncing-wallet.component.ts
+++ src/app/wallet/modals/syncing-wallet/syncing-wallet.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { RpcStateService } from '../../../core/core.module';
+import { ApiEndpoints, message } from '../../business-model/enums';
+import { Log } from 'ng2-logger';
+import { IGetblockchaininfo, getblockchaininfo } from '../../business-model/entities';
 
 @Component({
   selector: 'app-syncing-wallet',
@@ -8,14 +12,30 @@ import { Component, OnInit } from '@angular/core';
 export class SyncingWalletComponent implements OnInit {
 
   data: any;
-  constructor() { }
+  private log: any = Log.create(`SyncingWallet.component `);
+  private destroyed: boolean = false;
+  blockchaininfo: IGetblockchaininfo = new getblockchaininfo();
+  constructor(private _rpcState: RpcStateService) { }
 
   ngOnInit() {
+    this.getblockchaininformation();
   }
 
   setData(data: any) {
     this.data = data;
   }
 
+  //get block chain informations
+  private getblockchaininformation() {
+    this._rpcState.observe(ApiEndpoints.Getblockchaininfo)
+      .takeWhile(() => !this.destroyed)
+      .subscribe(blockchaininfo => {
+        this.blockchaininfo = new getblockchaininfo(blockchaininfo).toJSON();
+      },
+        error => this.log.error(message.walletMessage, error));
+  }
 
+  ngOnDestroy() {
+    this.destroyed = true;
+  }
 }
