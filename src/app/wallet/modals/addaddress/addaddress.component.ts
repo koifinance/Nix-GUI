a6655e2a5745069625e7dd765e119bd4e0ab66b7
silverstar222 <silverstar222@users.noreply.github.com>
2018-09-04 15:58:44 -0400

    data binding for Modal 1.3 - Receive NIX to Wallet > New address, 6.6 - Settings > Address Book > Add new, Modal 1.4 - Send NIX from Wallet > Confirmation, Modal 2.3 - Send NIX from Ghost Vault > Confirmation, added validations, services added for add address, entity added for get new address and set account
    data binding for Modal 1.3 - Receive NIX to Wallet > New address, 6.6 -
    Settings > Address Book > Add new,
    Modal 1.4 - Send NIX from Wallet > Confirmation, Modal 2.3 - Send NIX
    from Ghost Vault > Confirmation,
    added validations,
    services added for add address,
    entity added for get new address and set account

--- src/app/wallet/modals/addaddress/addaddress.component.ts
+++ src/app/wallet/modals/addaddress/addaddress.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { WalletService } from '../../wallet.service';
+import { message } from '../../business-model/enums';
+import { SnackbarService } from '../../../core/core.module';
+import { Log } from 'ng2-logger';
+import { ISetAccount, SetAccount } from '../../business-model/entities';
+
 @Component({
   selector: 'app-addaddress',
   templateUrl: './addaddress.component.html',
   styleUrls: ['./addaddress.component.scss']
 })
 export class AddaddressComponent implements OnInit {
-  faEyeSlash: any = faEyeSlash;
-  faEye: any = faEye;
-  showPassword: boolean = false;
-  constructor() { }
+  addAddress: ISetAccount = new SetAccount();
+  private log: any = Log.create('addaddress.component');
+
+  constructor(private walletServices: WalletService,
+    private flashNotification: SnackbarService) { }
 
   ngOnInit() {
   }
 
-  togglePassword() {
-    this.showPassword = !this.showPassword;
-  }
-
-  passwordLabelText(): string {
-    return this.showPassword ? 'Hide' : 'Show';
+  save() {
+    debugger
+    var result = this.walletServices.receiveNIXToWallet(this.addAddress).subscribe(res => {  
+      debugger
+   }, error => {
+    this.flashNotification.open(message.AddressAddedMessage, 'err')
+    this.log.er(message.AddressAddedMessage, error)
+  });
   }
 }
