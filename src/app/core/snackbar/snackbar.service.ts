5efb41e98aaa2311d8e84621f120601e2cf3b1ff
Mauro <mauroh.perk@gmail.com>
2018-10-10 17:34:40 +0200

    Feature Add - new notification system

--- src/app/core/snackbar/snackbar.service.ts
+++ src/app/core/snackbar/snackbar.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
+import { NotificationsService } from 'angular2-notifications';
 
 @Injectable()
 export class SnackbarService {
 
-  constructor(private snackBar: MatSnackBar) {
+  constructor(
+    private snackBar: MatSnackBar,
+    private notificationsService: NotificationsService
+  ) {
   }
 
   open(message: string, type?: string, action?: string): void {
@@ -14,7 +18,10 @@ export class SnackbarService {
       ['err', 'warn'].includes(type) ? 10000 :
       'info' === type ? 5000 : 2000);
 
-    this.snackBar.open(message, action ? action : 'Dismiss', config);
+    // this.snackBar.open(message, action ? action : 'Dismiss', config);
+    // if (type == 'err') this.notificationsService.error('Error', message)
+    if (type == 'err') this.notificationsService.warn('Warning', message)
+    else if (type == 'info') this.notificationsService.info('Info', message)
   }
 
 }
