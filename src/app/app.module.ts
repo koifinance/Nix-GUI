c198a8a82bebe76a6f6d252818f9e9c825545a52
Mauro <mauroh.perk@gmail.com>
2018-10-15 16:21:28 +0200

    Feature add - Loading indicator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SimpleNotificationsModule} from 'angular2-notifications';
 import { NgModule } from '@angular/core';
 import { Ng2CsvModule } from 'ng2csv';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { WalletModule } from './wallet/wallet.module';
 import { CoreModule } from './core/core.module';
@@ -26,6 +27,7 @@ import { ClipboardModule } from 'ngx-clipboard';
     HttpModule,
     ClipboardModule,
     Ng2CsvModule,
+    NgxSpinnerModule,
     SimpleNotificationsModule.forRoot()
   ],
   bootstrap: [AppComponent],
