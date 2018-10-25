c17e68f15ea87b5eb2cd89441a55ec8f5cfa5d44
rbfront <17902300+rbfront@users.noreply.github.com>
2018-10-25 22:41:03 +0300

    Update - version update, Settings button style update

--- src/app/wallet/updates/updates.component.ts
+++ src/app/wallet/updates/updates.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { RpcStateService } from '../../core/core.module';
+import { ApiEndpoints } from '../business-model/enums';
 
 @Component({
   selector: 'app-updates',
@@ -7,13 +9,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./updates.component.scss']
 })
 export class UpdatesComponent implements OnInit {
+  destroyed: boolean = false;
+  daemonVersion: string;
 
-  constructor(private router: Router) { }
+  constructor(private _rpcState: RpcStateService,private router: Router) { }
 
   ngOnInit() {
+  	this._rpcState.observe(ApiEndpoints.GetNetworkInfo).takeWhile(() => !this.destroyed)
+      .subscribe(networkInfo => {
+        let w_version = networkInfo.version.toString();
+        this.daemonVersion = w_version.slice(0, 1) + '.' + parseInt(w_version.slice(1, 3), 10) + '.' + parseInt(w_version.slice(3, 5), 10);
+      }, error => {
+        // this.log.d(error, 'err');
+      }
+    )
   }
   GoToDownlod() {
     this.router.navigate(['./main/downloading-updates']);
   }
 
+  ngOnDestroy() {
+    this.destroyed = true;   
+  }
 }
