b2f0f96429d52a851c43198b524c1a39fc646a2c
rbfront <17902300+rbfront@users.noreply.github.com>
2018-10-26 00:40:44 +0300

    Update - Version display update

--- src/app/wallet/main/main.component.ts
+++ src/app/wallet/main/main.component.ts
@@ -4,6 +4,7 @@ import { RpcStateService } from '../../core/core.module';
 import { ApiEndpoints } from '../business-model/enums';
 import { RpcService } from '../../core/rpc/rpc.service';
 import { ModalsService } from '../modals/modals.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'wallet-main',
@@ -12,34 +13,22 @@ import { ModalsService } from '../modals/modals.service';
 })
 export class MainComponent implements OnInit, OnDestroy {
 
-
   private log: any = Log.create('main.component');
+  public version: string = environment.version;
   routerLink: string;
  
   destroyed: boolean = false;
   syncProgress: number;
-  walletVersion: string;
+
   constructor(private modalsService: ModalsService, private _rpcState: RpcStateService, private _rpc: RpcService) {
   }
 
   ngOnInit() {
     this.routerLink = '';
-    // this._rpcState.registerStateCall(ApiEndpoints.Getblockchaininfo, 1000);
-
     this._rpcState.observe(ApiEndpoints.Getblockchaininfo).takeWhile(() => !this.destroyed)
       .subscribe(res => {
         this.syncProgress = Math.floor(res.verificationprogress * 100);
     });
-
-    this._rpcState.observe(ApiEndpoints.GetNetworkInfo).takeWhile(() => !this.destroyed)
-      .subscribe(networkInfo => {
-        let w_version = networkInfo.version.toString();
-        this.walletVersion = w_version.slice(0, 1) + '.' + parseInt(w_version.slice(1, 3), 10) + '.' + parseInt(w_version.slice(3, 5), 10);
-      }, error => {
-        this.log.d(error, 'err');
-      }
-    )
-
   }
 
   openSyncingWallet() {
